90678b2570390d36571465b2f624f29f954d584c
Lucas Lins <lucaslinsperfil@gmail.com>
2022-06-13 15:49:49 -0300

    atualização do app

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <StatusBar style="auto"  />
+    <View style={{display:flex,flexDirection:'row'}}>
+        <Image
+        source={{uri:'https://picsum.photos/200/300'}}
+        style={styles.postImage}    
+      />
+        <View style={styles.postContentWrap}>
+          <View>
+          <Text style={{fontWeight:"bold"}}>This is our core project</Text>
+          <Text style={{color:"grey"}}>New Your</Text>
+          </View>
+        <Text style={{color:"#fec85c",backgroundColor:"#293141",alignSelf:"flex-start",padding:5,borderRadius:10,}}>$100 / Day</Text>
+        </View>
+    </View>
     </View>
   );
 }
@@ -16,5 +28,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor:"#fec85c",
   },
+  postImage:{
+    height:100,
+    width:100,
+    marginLeft:20,
+    marginRight:20,
+    marginVertical:10,  
+  },
+  postContentWrap:{justifyContent:"space-around",paddingVertical:10}
 });
